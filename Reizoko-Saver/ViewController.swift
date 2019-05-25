1be70fbdfefa882fad0db115e433a95f8d37ea54
SatoshiOkada <soysoysoup@gmail.com>
2019-05-25 12:39:19 +0900

    Initial Commit

---
+++ Reizoko-Saver/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Reizoko-Saver
+//
+//  Created by 岡田聡 on 2019/05/25.
+//  Copyright © 2019 SatoshiOkada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Reizoko-SaverTests/Reizoko_SaverTests.swift
@@ -0,0 +1,34 @@
+//
+//  Reizoko_SaverTests.swift
+//  Reizoko-SaverTests
+//
+//  Created by 岡田聡 on 2019/05/25.
+//  Copyright © 2019 SatoshiOkada. All rights reserved.
+//
+
+import XCTest
+@testable import Reizoko_Saver
+
+class Reizoko_SaverTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
