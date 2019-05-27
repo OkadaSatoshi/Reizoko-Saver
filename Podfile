5619de5556a99ce22a7d8c8e102fe02d3a540359
SatoshiOkada <soysoysoup@gmail.com>
2019-05-27 17:04:54 +0900

    pod 導入

---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '12.2'
+
+target 'Reizoko-Saver' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Reizoko-Saver
+  pod 'RxSwift'
+  pod 'RealmSwift'
+
+  target 'Reizoko-SaverTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'Reizoko-SaverUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- Reizoko-Saver/Model/Item.swift
+++ Reizoko-Saver/Model/Item.swift
@@ -8,22 +8,31 @@
 
 import Foundation
 import UIKit
+import RealmSwift
 
-struct Item {
-    var name: String
-    var number: ItemNumber
-    var image: UIImage
-    var purchaseDate: Date
-    var expirationDate: Date
+class Item: Object {
+    dynamic var name: String = ""
+    dynamic var number: ItemNumber = ItemNumber()
+    dynamic var image: UIImage?
+    dynamic var purchaseDate: Date = Date()
+    dynamic var expirationDate: Date = Date()
 }
 
-struct ItemNumber {
-    var number: Int = 0
-    var unitType: UnitType
+class ItemNumber: Object {
+    dynamic var number: Int = 0
+    dynamic private var unit = ""
+    var unitType: UnitType? {
+        get {
+            return UnitType(rawValue: unit)
+        }
+        set {
+            unit = newValue?.rawValue ?? ""
+        }
+    }
 }
 
-enum UnitType {
-    case gram
+enum UnitType: String {
+    case gram = "g"
     case cm
     case ml
 }
