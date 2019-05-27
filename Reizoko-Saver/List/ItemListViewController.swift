fc7731d8f1a7dc8999a5c4578790f86872f3c18e
SatoshiOkada <soysoysoup@gmail.com>
2019-05-27 15:20:08 +0900

    Tabbarと子どものviewControllerの設定まで

---
+++ Reizoko-Saver/List/ItemListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ItemListViewController.swift
+//  Reizoko-Saver
+//
+//  Created by 岡田聡 on 2019/05/27.
+//  Copyright © 2019 SatoshiOkada. All rights reserved.
+//
+
+import UIKit
+
+class ItemListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Reizoko-Saver/MainTabBarViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainTabBarViewController.swift
+//  Reizoko-Saver
+//
+//  Created by 岡田聡 on 2019/05/27.
+//  Copyright © 2019 SatoshiOkada. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
